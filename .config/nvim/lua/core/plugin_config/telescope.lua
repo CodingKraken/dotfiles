8c810e2f3761f5f42d22e9c5326f9c611caca6b2
CodingKraken <52387931+CodingKraken@users.noreply.github.com>
2024-08-16 19:18:21 -0600

    Delete .config/nvim directory

--- .config/nvim/lua/core/plugin_config/telescope.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader><space>', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
